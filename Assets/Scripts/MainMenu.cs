d73065e8b66dfa44587abab1b353a0cd42e73715
Noah <Noah_castro25@yahoo.com>
2021-05-05 16:08:29 -0700

    SettingsPage Added, including more functionality

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{ 
+
+    // public GameObject canvas; 
+
+    public void StartButton() {
+        SceneManager.LoadScene("MainHub");
+    }
+
+    public void HomeButton() {
+        SceneManager.LoadScene("MainHub");
+    }
+
+    public void AccountButton() {
+        SceneManager.LoadScene("AccountPage");
+    }
+
+    public void CalendarButton() {
+        SceneManager.LoadScene("CalendarPage");
+    }
+
+    public void SettingsButton() {
+        SceneManager.LoadScene("SettingsPage");
+    }
+}
